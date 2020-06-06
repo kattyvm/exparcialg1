97bfffdffd4b9e451a61a2f4e75baa4f32a953aa
AlonsoGuevara15 <a20160679@pucp.edu.pe>
2020-06-05 20:17:08 -0500

    templates

--- src/main/java/exparcialg1/demo/Controller/MainController.java
+++ src/main/java/exparcialg1/demo/Controller/MainController.java
@@ -1,6 +1,7 @@
 package exparcialg1.demo.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping(value = {"/"})
 public class MainController {
     @GetMapping(value = {"/"})
-    public String index(){
+    public String index(Model m){
+        int num=4;
+        m.addAttribute("num",num);
         return "donpepe/pgPrincipal";
     }
 }
--- src/main/java/exparcialg1/demo/Controller/VistasController.java
+++ src/main/java/exparcialg1/demo/Controller/VistasController.java
@@ -1,6 +1,7 @@
 package exparcialg1.demo.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class VistasController {
 
     @GetMapping(value = {"/{page}"})
-    public String index(@PathVariable String page){
+    public String index(@PathVariable String page, Model m){
+        int num=1;
+        m.addAttribute("num",num);
         return "donpepe/" +page;
     }
 }
