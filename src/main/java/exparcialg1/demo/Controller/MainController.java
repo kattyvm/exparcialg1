884ddac9828695bf10bd73e636a6e22ef6fed324
AlonsoGuevara15 <a20160679@pucp.edu.pe>
2020-06-05 19:12:23 -0500

    templates

--- src/main/java/exparcialg1/demo/Controller/MainController.java
+++ src/main/java/exparcialg1/demo/Controller/MainController.java
@@ -2,15 +2,13 @@ package exparcialg1.demo.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping(value = {"/",""})
+@RequestMapping(value = {"/"})
 public class MainController {
-
-    @GetMapping(value = {"/{page}"})
-    public String index(@PathVariable String page){
-        return "donpepe/"+page;
+    @GetMapping(value = {"/"})
+    public String index(){
+        return "donpepe/pgPrincipal";
     }
 }
---
+++ src/main/java/exparcialg1/demo/Controller/VistasController.java
@@ -0,0 +1,16 @@
+package exparcialg1.demo.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = {"/vistas"})
+public class VistasController {
+
+    @GetMapping(value = {"/{page}"})
+    public String index(@PathVariable String page){
+        return "donpepe/" +page;
+    }
+}
