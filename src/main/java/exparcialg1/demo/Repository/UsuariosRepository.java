9b68f01beac6b176d63e20644a4f97c2a3aa8f7c
katty <a20160555@pucp.edu.pe>
2020-06-06 20:17:48 -0500

    validaciones admin

--- src/main/java/exparcialg1/demo/Repository/UsuariosRepository.java
+++ src/main/java/exparcialg1/demo/Repository/UsuariosRepository.java
@@ -2,6 +2,7 @@ package exparcialg1.demo.Repository;
 
 import exparcialg1.demo.Entity.UsuariosEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,6 +14,17 @@ public interface UsuariosRepository extends JpaRepository<UsuariosEntity,Integer
     public UsuariosEntity findUsuariosEntityByCorreo(String correo);
 
     List<UsuariosEntity> findUsuariosEntityByRol_idroles(int rol);
+
+    @Query(value="SELECT idusuarios, nombre, apellido, dni, SUBSTRING_INDEX(correo, '@', 1) as correo, pwd, enabled, idroles " +
+            "FROM usuarios WHERE idusuarios = ?",
+            nativeQuery = true)
+    public Optional<UsuariosEntity> obtenerUsuarioCorreo(int id);
+
+    //List<UsuariosEntity> findUsuariosEntityByDni(int dni);
+
+    @Query(value="CALL saveGestor(?, ?, ?, ?, ?, ?, ?)",
+            nativeQuery = true)
+    public String saveGestor(int id, String nombre, String apellido, int dni, String correo, Boolean activ, int rol);
 
 
 }
