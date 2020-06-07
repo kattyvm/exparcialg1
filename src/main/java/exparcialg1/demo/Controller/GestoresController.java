325a8d65f8c5aa53c05d228e5f27b720a9616bc4
allisonvp <a.velardep@pucp.edu.pe>
2020-06-06 19:22:36 -0500

    codigo generado y validaciones

--- src/main/java/exparcialg1/demo/Controller/GestoresController.java
+++ src/main/java/exparcialg1/demo/Controller/GestoresController.java
@@ -4,6 +4,7 @@ import exparcialg1.demo.Entity.ProductosEntity;
 import exparcialg1.demo.Repository.ProductosRepository;
 import exparcialg1.demo.Repository.UsuariosRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
-import java.util.Optional;
+import java.util.*;
 import java.util.regex.Pattern;
 
 @Controller
@@ -23,30 +24,31 @@ public class GestoresController {
     @Autowired
     ProductosRepository productosRepository;
 
-    @GetMapping(value = {"listaProdDispPaginaPrincipal","/"})
-    public String listaProdDispGestion(Model model){
-        model.addAttribute("listaProductosDisponibles",productosRepository.findAll());
+    @GetMapping(value = {"listaProdDispPaginaPrincipal", "/"})
+    public String listaProdDispGestion(Model model) {
+        model.addAttribute("listaProductosDisponibles", productosRepository.findAll());
         return "donpepe/index";
     }
 
-    @GetMapping(value ="listaProdGestion")
-    public String listaProdDispPaginaPrincipal(Model model){
-        model.addAttribute("listaProductos",productosRepository.findAll());
+    @GetMapping(value = "listaProdGestion")
+    public String listaProdDispPaginaPrincipal(Model model) {
+        model.addAttribute("listaProductos", productosRepository.findAll());
         return "gestor/listaProductos";
     }
 
     @GetMapping(value = {"/formulario"})
-    public String formulario(@ModelAttribute("producto") ProductosEntity producto, Model model) {
+    public String formulario(@ModelAttribute("producto") ProductosEntity producto) {
+
         return "gestor/formulario";
     }
 
     @GetMapping("/editar")
     public String editarProducto(@ModelAttribute("producto") ProductosEntity producto,
-                                      Model model, @RequestParam("id") String id) {
+                                 Model model, @RequestParam("id") String id) {
 
         Optional<ProductosEntity> optProducto = productosRepository.findById(id);
         if (optProducto.isPresent()) {
-            producto= optProducto.get();
+            producto = optProducto.get();
             model.addAttribute("producto", producto);
             return "gestor/formulario";
         } else {
@@ -56,7 +58,7 @@ public class GestoresController {
 
     @PostMapping("/guardar")
     public String guardarProducto(@ModelAttribute("producto") @Valid ProductosEntity producto,
-                          BindingResult bindingResult, RedirectAttributes attr, Model model) {
+                                  BindingResult bindingResult, RedirectAttributes attr, Model model) {
 
         if (!producto.getNombre().isEmpty()) {
             if (Pattern.compile("[0-9]").matcher(producto.getNombre()).find()) {
@@ -67,17 +69,46 @@ public class GestoresController {
             }
         }
 
-        if(bindingResult.hasErrors()){
-            model.addAttribute("listaProductosDisponibles", productosRepository.findAll());
+        //Generacion del codigo
+        String nom = producto.getNombre();
+        char[] nomArreglo = nom.toCharArray();
+        String primero = (Character.toString(nomArreglo[0])).toUpperCase();
+        String ultimo = (Character.toString(nomArreglo[nomArreglo.length - 1])).toUpperCase();
+        List<ProductosEntity> listProductos = productosRepository.findAll();
+        int ultNum = listProductos.size() + 1;
+        String cad = Integer.toString(ultNum);
+        while (cad.length() < 4) {
+            cad = "0" + cad;
+        }
+
+
+        for (ProductosEntity prodTienda : listProductos) {
+
+            if (prodTienda.getNombre().equals(producto.getNombre())) {
+                if (producto.getCodproducto() == null) {
+                    bindingResult.rejectValue("nombre", "error.user", "El nombre de este producto ya existe.");
+                } else {
+                    if (!prodTienda.getCodproducto().equals(producto.getCodproducto())) {
+                        bindingResult.rejectValue("nombre", "error.user", "El nombre de este producto ya existe.");
+                    }
+                }
+            }
+
+        }
+
+        if (ultimo.equals(" ") || primero.equals(" ")) {
+            bindingResult.rejectValue("nombre", "error.user", "No se permiten espacios al inicio o al final del nombre.");
+        }
+
+        if (bindingResult.hasErrors()) {
             model.addAttribute("msg", "Error al ingresar los datos, verifique que los datos sean validos.");
             return "gestor/formulario";
-        }
-        else {
+        } else {
             Optional<ProductosEntity> optProducto = productosRepository.findById(producto.getCodproducto());
             if (optProducto.isPresent()) {
                 attr.addFlashAttribute("msg", "Producto actualizado exitosamente");
-            }
-            else {
+            } else {
+                producto.setCodproducto(primero + cad + ultimo);
                 attr.addFlashAttribute("msg", "Producto creado exitosamente");
             }
             productosRepository.save(producto);
